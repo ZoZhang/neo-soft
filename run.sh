302290fd3045d7bd4fff96344733455007c6c1a0
ZoZhang <Zo.Zhang@gmail.com>
2018-12-07 02:26:41 +0100

    Supprime des informations nulles

--- run.sh
+++ run.sh
@@ -16,47 +16,6 @@ if [ "$1" == php-fpm ] || [ "$1" == supervisord ] ; then
 	mkdir -p /var/nginx/client_body_temp;
 
     chown -R www-data:www-data /var/www/html/ /var/nginx/client_body_temp
-
-	#"${mysql[@]}" < "/tmp/exemple.sql";
-
-#mysql -uroot -p <<EOF
-#source /tmp/exemple.sql;
-#EOF
-  #   mysql -u root --password="" <<-EOSQL
-		# 	DROP DATABASE IF EXISTS test ;
-		# 	CREATE DATABASE IF NOT EXISTS film;
-		# 	USE film;
-		# 	SOURCE /tmp/exemple.sql;
-		# EOSQL
-
-		#rm -rf /tmp/exemple.sql;
 fi
-
-# if [ "$1" == mysql ] ; then
-
-# 	#Initialize mysql service
-# 	#if [ ! -d /var/lib/mysql/mysql ]; then
-# 	    #/usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=mysql &
-# 	    #sleep 5s
-
-# 		mysql -u root --password="" <<-EOSQL
-# 			DROP DATABASE IF EXISTS test ;
-# 			CREATE DATABASE IF NOT EXISTS film;
-# 			USE film;
-# 			SOURCE /tmp/exemple.sql;
-# 		EOSQL
-
-# 	 #    mysql -u root --password="" <<-EOSQL
-# 		# 	DROP DATABASE IF EXISTS test ;
-# 		# 	CREATE DATABASE IF NOT EXISTS film;
-# 		# 	USE film;
-# 		# 	SOURCE /tmp/exemple.sql;
-# 		# EOSQL
-
-# 		rm -rf /tmp/exemple.sql;
-
-# 		#killall -9 mysqld_safe
-# 	#fi
-# fi
 
 exec "$@"
